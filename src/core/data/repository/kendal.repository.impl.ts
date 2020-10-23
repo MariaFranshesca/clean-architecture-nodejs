c14506e3d2a3dfc33cddcb23a15a3fb3d29990c6
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2020-10-22 22:30:54 -0500

    update

--- src/core/data/repository/kendal.repository.impl.ts
+++ src/core/data/repository/kendal.repository.impl.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@nestjs/common"
 import { ClassKendalDto } from "src/core/domain/entities/dto/kendal.dto"
 import { IKendal } from "src/core/domain/entities/interfaces/kendal.interfaces"
+import { KendalRepository } from "src/core/domain/repository/kendal.repository"
 import { KendalDataSource } from "../source/kendal.datasource"
 
 @Injectable()
-export class KendalRepositoryImpl {
+export class KendalRepositoryImpl implements KendalRepository {
+  
   constructor(private kendalDataSource: KendalDataSource) {}
 
   async create(createKendalDto: ClassKendalDto): Promise<IKendal> {
