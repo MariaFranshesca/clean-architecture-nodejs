dfb2ea5eebbcfe06565f77c36acbf46273ed6427
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2020-11-14 19:29:47 -0500

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/modules/app.module'
-import { setupSwagger } from './swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.setGlobalPrefix('api')
-  setupSwagger(app)
   app.listenAsync(process.env.PORT || 3000)
 }
 bootstrap()
